ed3c518fb23e9b7871681a0b53dfc9279352fbff
David Codner <dccodner24@gmail.com>
2022-12-07 02:34:43 -0600

    Added repo links per requirements, Added resume section with a viewable resume, added professinal README.md

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# React-Portfolio
-A newer version of my personal portfolio powered by React.js
+# React Portfolio 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+ 
+## Description  
+<a name="descrip"></a>
+A personal portfolio powered by React.js 
+ 
+## Table of Contents 
+- [Description](#description)
+
+- [Installation Instructions](#instructions) 
+
+- [Usage Info](#usage) 
+
+- [Contribution Guidelines](#contributions) 
+
+- [Test Instructions](#tests) 
+
+- [License](#license) 
+
+- [Questions](#questions) 
+
+ 
+## Installation Instructions 
+<a name="instr"></a> 
+ 
+>N/A 
+ 
+## Usage information 
+<a name="usage"></a>  
+ 
+>To use this application, navigate to the provided [live-site link](https://dcodner24.github.io/React-Portfolio/) and interact with the page. 
+ 
+## Contribution Guidelines 
+<a name="contribution"></a>  
+ 
+>N/A 
+ 
+## Test Instructions 
+<a name="testing"></a>  
+ 
+>N/A 
+ 
+## License 
+<a name="licence"></a>  
+>This software is licensed under the MIT License, for further information or to read the documentation, visit (https://opensource.org/licenses/MIT). 
+ 
+## Questions 
+<a name="questions"></a> 
+
+>If you have any questions please check the issue for the associated repo at my github account: dcodner24 
+
+>For any additional questions, feel free to contact me at my work email: dccodner24@gmail.com
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,12 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
 import projImg2 from "../assets/img/collabProjects/codeBank.png";
-import projImg1 from "../assets/img/collabProjects/parkPicker.jpg";
+import projImg1 from "../assets/img/collabProjects/parkPicker.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
+
+
 
 export const Projects = () => {
 
@@ -21,6 +23,19 @@ export const Projects = () => {
     }
   ];
 
+  const repoLinks = [
+    {
+      description: "Park Picker: An interactive webapp for exploring National Parks across the United States.",
+      imgUrl: projImg1,
+      liveSite: "https://github.com/chewy441014/trail-picker"
+    },
+    {
+      description: "A user-friendly mock banking application that allows users to easily deposit, pay, and transfer funds between accounts on the platform.",
+      imgUrl: projImg2,
+      liveSite: "https://github.com/dcodner24/Code-Bank"
+    }
+  ];
+
   return (
     <section className="project" id="project">
       <Container>
@@ -28,46 +43,60 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>These are the projects I have completed so far, feel free to browse through them too see the different technologies I have worked with.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Collaborative Projects</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Solo Projects</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Personal Projects</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          collabProjects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>These are the projects I have completed so far, feel free to browse through them too see the different technologies I have worked with.</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                      <Nav.Item>
+                        <Nav.Link eventKey="first">Projects</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="second">Repos</Nav.Link>
+                      </Nav.Item>
+                      <Nav.Item>
+                        <Nav.Link eventKey="third">Resume</Nav.Link>
+                      </Nav.Item>
+                    </Nav>
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {
+                            collabProjects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
                                 />
-                                
-                            )
-                          })
+
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="second">
+                        <Row>
+                          {
+                            repoLinks.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
+                                />
+
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                      <Tab.Pane eventKey="third">
+                        {
+                          <button className="resumeButton" onClick={() => window.open("https://drive.google.com/file/d/18JzCV5uOQnlPVqi-ljCcwuKjpmgDL9v-/view?usp=share_link")}><span>View Resume</span></button>
                         }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="second">
-                    <h3>More Coming Soon!</h3>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <h3>More Coming Soon!</h3>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
